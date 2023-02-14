92f65ea7961d07c2aa85f772fc43606fdbfa1554
Von Random <von@vdrandom.org>
2023-02-14 16:46:42 +0200

    version 1.10

--- sshpass.rb
+++ sshpass.rb
@@ -1,8 +1,8 @@
 class Sshpass < Formula
   desc "This is a tool for non-interactivly performing password authentication with SSH"
   homepage "https://sourceforge.net/projects/sshpass"
-  url "https://udomain.dl.sourceforge.net/project/sshpass/sshpass/1.09/sshpass-1.09.tar.gz"
-  sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
+  url "https://udomain.dl.sourceforge.net/project/sshpass/sshpass/1.10/sshpass-1.10.tar.gz"
+  sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
   license "GPL-2.0-or-later"
 
   def install
